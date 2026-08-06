2dee60c9fd73789a3e206e46ce77bcbd0725645b
EDSON HENRIQUE ABREU DE SOUZA <edson.souza23@senaisp.edu.br>
2026-08-06 20:26:39 -0300

    Adição do projeto padrão do backend em Springboot

---
+++ back/src/main/java/br/com/healthplanner/back/controller/PatientController.java
@@ -0,0 +1,29 @@
+package br.com.healthplanner.back.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.healthplanner.back.dto.PatientDTO;
+
+@RestController
+@RequestMapping("/api")
+public class PatientController {
+
+    public ArrayList<PatientDTO> patients = new ArrayList<>();
+
+    @RequestMapping("/patients")
+    public ArrayList<PatientDTO> showPatients() {
+        return patients;
+    }
+    
+    @RequestMapping
+    public PatientDTO insertPatient(@RequestBody PatientDTO patient) {
+        patients.add(patient);
+        return patient;
+    }
+    
+    
+}
---
+++ back/src/main/java/br/com/healthplanner/back/dto/DoctorDTO.java
@@ -0,0 +1,25 @@
+package br.com.healthplanner.back.dto;
+
+public class DoctorDTO {
+    private Long id;
+    private String name;
+
+    public DoctorDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
